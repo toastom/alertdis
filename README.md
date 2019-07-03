8e7687fa122c9e5d9c0ab3dd31240054dd2d8554
Thomas <thomas.gourley25@gmail.com>
2019-07-03 00:51:53 -0400

    Basic beginning

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 alertdis
+
+Discord Bot written in Python 3 to help manage server alerts and automate messages.
---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord, os, time, asyncio
+from discord.ext import commands
+
+
+BOT_PREFIX = '>'
+TOKEN = os.environ['token']
+
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print('discord version:')
+    print(discord.__version__)
+    print('------')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    servers = list(bot.guilds)
+    print('Connected on '+ str(len(servers)) + ' servers:')
+    print('\n'.join('>'+ server.name for server in servers))
+    print('------')
+
+@bot.commands(name="alert")
+async def alert(ctx):
+    pass
+
+
+bot.run(TOKEN)
