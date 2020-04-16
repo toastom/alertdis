cd862a7a027cf752fb5a6fe7a8d76844eb6ee5c4
Thomas <thomas.gourley25@gmail.com>
2020-04-16 01:23:40 -0400

    Not sure

---
+++ alert.py
@@ -0,0 +1,5 @@
+class Alert():
+    def __init__(self, guild=None, name="", secs=0):
+        self.guild = guild
+        self.name  = name
+        self.secs  = secs
--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-import discord, os, time, asyncio
+import discord, os, time, asyncio, re
 from discord.ext import commands
 
+import alert
 
-BOT_PREFIX = '>'
+
+BOT_PREFIX = os.environ['prefix']
 TOKEN = os.environ['token']
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+alerts = []
 
 @bot.event
 async def on_ready():
@@ -21,9 +25,28 @@ async def on_ready():
     print('\n'.join('>'+ server.name for server in servers))
     print('------')
 
-@bot.command(name="alert")
-async def alert(ctx):
-    await ctx.message.channel.send("alerted")
+@bot.command(name="setalert")
+async def set_alert(ctx, name="", hours=0, mins=0, secs=0):
+    #await ctx.message.channel.send("alerted")
+    #pattern = re.compile(r'')
+    #match = pattern.finditer(time)
+    if(name == ""):
+        await ctx.message.channel.send("Must input a name for the alert")
+        return
+
+    if(hours == 0 and mins == 0 and secs == 0):
+        await ctx.message.channel.send("Must input hours, minutes, and seconds")
+        return
+    
+    secs = (hours * 60 * 60) + (mins * 60) + secs
+
+    new_alert = alert.Alert(ctx.message.guild, name, secs)
+    alerts.append(new_alert)
+    print(new_alert, alerts)
+    print("{} hours, {} mins, {} secs".format(hours, mins, secs))
+    await ctx.message.channel.send(":white_check_mark: Alert {} successfully registered.".format(name))
+
+
 
 
 bot.run(TOKEN)
